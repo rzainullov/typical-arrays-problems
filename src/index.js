27f371c42cf48ee976b32c3f969f7ecfccb503c9
rzainullov <rashid.zainullov@gmail.com>
2020-09-30 10:58:07 +0400

    init: start task and finsh task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = '') {
+  if(array.length == 0 || array == '') return 0;
+  if(array.length == 1) return array[0];
+  let minValue = parseFloat(array[0]);
+  for(let i = 1; i < array.length;i++){
+    if(array[i] < minValue){
+      minValue = array[i];
+    } 
+  }
+  return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = '') {
+  if(array.length == 0 || array == '') return 0;
+  if(array.length == 1) return array[0];
+  let maxValue = parseFloat(array[0]);
+  for(let i = 1; i < array.length;i++){
+    if(array[i] > maxValue){
+      maxValue = array[i];
+    } 
+  }
+  return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = '') {
+  if(array.length == 0 || array == '') return 0;
+  if(array.length == 1) return array[0];
+  let sum = 0;
+  for(let i = 0; i < array.length;i++){
+    sum += parseFloat(array[i])
+  }
+  return sum/array.length
 }
